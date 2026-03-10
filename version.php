c04b19c063339542775070ff1c5674ba594cee6a
Luiggi Sansonetti <1565841+luiggisanso@users.noreply.github.com>
2026-03-10 12:07:52 +0100

    Update version.php

--- version.php
+++ version.php
@@ -27,10 +27,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_plugininstalltimer';
-$plugin->version   = 2026030900;
+$plugin->version   = 2026031000;
 $plugin->requires  = 2024100700; // Moodle 4.5+
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.0';
 $plugin->supported = [405, 501];
-
-
